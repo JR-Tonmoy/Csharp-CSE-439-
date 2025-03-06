e8c2c63734684420f17c4c9751b804c408efcb1c
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-06 23:59:43 +0600

    Add files via upload

---
+++ Assignment Operators/Assignment Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.Probler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            number += 5; //number=number+8;
+            Console.WriteLine($"{number}");
+
+
+            number -= 5;//number=number-5;
+            Console.WriteLine($"{number}");
+
+            number *= 5; //number=number*5;
+            Console.WriteLine($"{number}");
+
+            number /= 5; //number=number/5;
+            Console.WriteLine($"{number}");
+        }
+    }
+}
+
