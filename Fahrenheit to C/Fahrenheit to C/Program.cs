2e9776d6573d5ae83626251036aee7651fb046f4
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-06 20:41:21 +0600

    Add files via upload

---
+++ Fahrenheit to C/Fahrenheit to C/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrenheit_to_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit, Celsius;
+            Console.Write("Fahrenheit:");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            Celsius = (fahrenheit - 32) / 1.8;
+             Console.WriteLine($"Celsius={ Celsius:F2}");
+           // Console.WriteLine($"celsius={celsius.ToString("F2")}");
+        }
+    }
+}
