0f46533645be73d60b800ae03d9a27193ca94154
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 20:32:14 +0600

    Add files via upload

---
+++ Letter grade, nested if/Letter grade, nested if/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Letter_grade_nested_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double marks;
+            Console.Write("Enter the Mark: ");
+            marks = Convert.ToDouble(Console.ReadLine());
+
+            if (marks >= 0 && marks <= 100)
+            {
+                if (marks >= 90)
+                {
+                    Console.WriteLine("A");
+                }
+                else if (marks >= 80)
+                {
+                    Console.WriteLine("B");
+                }
+                else if (marks >= 70)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (marks >= 60)
+                {
+                    Console.WriteLine("D");
+                }
+                else
+                {
+                    Console.WriteLine("F");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{marks} is an Invalid input. Please enter a value between 0 and 100.");
+            }
+        }
+    }
+}
