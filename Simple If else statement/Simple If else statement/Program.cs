9637ac811a5ee699e5db3ede516c6cc686f7d5ab
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 16:25:39 +0600

    Add files via upload

---
+++ Simple If else statement/Simple If else statement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simple_If_else_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 0;
+            int number;
+            Console.Write("Enter the number:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("Positive Number");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Negative Number");
+            }
+            else
+            {
+                Console.WriteLine($"The ans is :{number}");
+            }
+        }
+    }
+}
