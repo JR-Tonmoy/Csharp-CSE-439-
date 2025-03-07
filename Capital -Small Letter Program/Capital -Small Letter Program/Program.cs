1f73198e6f2bd0248a1b50d14b81d4c537821a9b
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 21:57:52 +0600

    Add files via upload

---
+++ Capital -Small Letter Program/Capital -Small Letter Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital__Small_Letter_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            Console.Write("Enter the letter:");
+            letter = Convert.ToChar(Console.ReadLine());
+
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                Console.WriteLine($"{letter} is Capital");
+            }
+            else
+            {
+                Console.WriteLine($"{letter} is Small");
+            }
+        }
+    }
+}
