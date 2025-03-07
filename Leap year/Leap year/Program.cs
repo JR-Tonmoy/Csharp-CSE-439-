c7348d65af2b6fc087b1a0c6821f3d712d3691a2
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 21:39:17 +0600

    Add files via upload

---
+++ Leap year/Leap year/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter year:");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"{year} is Leap year");
+            }
+            else if (year % 4==0 && year %100 != 0)
+            {
+                Console.WriteLine($"{year} is Leap year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not Leap year");
+            }
+        }
+    }
+}
