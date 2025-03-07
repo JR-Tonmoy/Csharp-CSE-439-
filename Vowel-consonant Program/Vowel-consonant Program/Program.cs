7fb36a8c8952d0f90121a793369a1560b0072e7f
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 22:14:16 +0600

    Add files via upload

---
+++ Vowel-consonant Program/Vowel-consonant Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_consonant_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+            {
+                char letter;
+                Console.Write("Enter the letter:");
+                letter = Convert.ToChar(Console.ReadLine());
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ) 
+                {
+                    Console.WriteLine($"{letter} is Vowel");
+                }
+                else
+                {
+                    Console.WriteLine($"{letter} is consonant");
+                }
+            }
+     
+    }
+}
