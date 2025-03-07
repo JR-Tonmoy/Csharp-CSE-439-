4cf5950cafa0288e275a8526efde9aa7e080fefc
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 12:58:49 +0600

    Add files via upload

---
+++ Unary Operators/Unary Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unary_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 15;
+            Console.WriteLine(++number);//16
+           Console.WriteLine(++number);//17
+            Console.WriteLine(number++);//17
+            Console.WriteLine(number);//18
+            Console.WriteLine(--number);//17
+            Console.WriteLine(number--);//17
+            Console.WriteLine(number--);//16
+            Console.WriteLine(number);
+        }
+    }
+}
