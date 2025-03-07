d495c5012ed7408146d84825eb0294bad17802f7
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-03-07 19:20:34 +0600

    Add files via upload

---
+++ Large number-Small number/Large number-Small number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Large_number_Small_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.Write("Enter the number1:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number2:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1>number2)
+            {
+                Console.WriteLine($"{number1} is Larhe number. ");
+            }
+            else if(number1<number2)
+            {
+                Console.WriteLine($"{number1} is Small number.");
+            }
+            else 
+            {
+                Console.WriteLine($"numbers are equal.");
+            }
+        }
+    }
+}
