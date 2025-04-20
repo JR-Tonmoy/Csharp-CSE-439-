4306c9de816f929432166c05823c2459c18c5b96
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-04-20 23:44:44 +0600

    Add files via upload

---
+++ switch statement-vowel consonant/switch statement-vowel consonant/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statement_vowel_consonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            Console.Write("Enter a sihgle character:");
+            letter = Convert.ToChar(Console.ReadLine());
+
+            switch(char.ToLower(letter))
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine($"{letter} is vowel.");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid character");
+                    break;
+                
+            }
+
+        }
+    }
+}
