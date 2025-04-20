10941d6acd53dc24cf04170776ae58ef1cc2e21c
Tonmoy Sarker <tonmoysarker7676@gmail.com>
2025-04-21 00:23:59 +0600

    Add files via upload

---
+++ Weekday or Weekend/Weekday or Weekend/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weekday_or_Weekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a day of the Week:");
+            String day = Console.ReadLine();
+            switch (day)
+            {
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    Console.WriteLine("This is Weekday");
+                    break;
+                case "Sunday":
+                case "monday":
+                    Console.WriteLine("This is Weekend");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day entered");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Weekday or Weekend/Weekday or Weekend/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Weekday or Weekend")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Weekday or Weekend")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("188c18d8-d416-4676-9fff-d3fb30133136")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
